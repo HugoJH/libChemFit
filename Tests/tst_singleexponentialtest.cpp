92fec738031b189569740865e1631da2223579ba
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2018-02-12 09:55:10 +0100

    Modifying test function name according to functionality

--- Tests/tst_singleexponentialtest.cpp
+++ Tests/tst_singleexponentialtest.cpp
@@ -1,5 +1,7 @@
 #include <QString>
 #include <QtTest>
+#include
+
 
 class SingleExponentialTest : public QObject
 {
@@ -8,7 +10,7 @@ class SingleExponentialTest : public QObject
    public:
       SingleExponentialTest();
    private Q_SLOTS:
-      void testgetFirstPreParameter();
+      void testcomputeFirstPreParameter();
 };
 
 SingleExponentialTest::SingleExponentialTest()
@@ -16,11 +18,11 @@ SingleExponentialTest::SingleExponentialTest()
 
 }
 
-void SingleExponentialTest::testgetFirstPreParameter()
+void SingleExponentialTest::testcomputeFirstPreParameter()
 {
    QVector<double> X= {1,2,3,4,5};
    QVector<double> Y= {1,3,5,7,9};
-   double P1 = getFirstParameter(X,Y);
+   double P1 = computeFirstPreParameter(X,Y);
    QVERIFY2(P1 == 5.3, "Computation of single exponential first pre-parameter failed");
 }
 
