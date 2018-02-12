1567d84157fabdaff27087a7ec79f1e40bf8c8f2
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2018-02-12 16:30:44 +0100

    Various operator overloading

--- source/SingleExponential.h
+++ source/SingleExponential.h
@@ -9,6 +9,45 @@ class SingleExponential
       SingleExponential();
 
       static double computeFirstPreParameter(const QVector<double> & X, const QVector<double> & Y);
+
 };
+
+QVector<double> operator+(const QVector<double> & X, const QVector<double> & Y)
+{
+   QVector<double> temp;
+
+   for (int i = 0; i < X.count(); ++i)
+   {
+        temp << Y[i] + X[i];
+   }
+   return temp;
+}
+
+QVector<double> operator+(const QVector<double> & X, double n)
+{
+   QVector<double> temp;
+
+   for (int i = 0; i < X.count(); ++i)
+   {
+        temp << X[i] + n;
+   }
+   return temp;
+}
+
+QVector<double> operator+(double n, const QVector<double> & X)
+{
+   return operator +(X, n);
+}
+
+QVector<double> operator*(const QVector<double> & X, const QVector<double> & Y)
+{
+   QVector<double> temp;
+
+   for (int i = 0; i < X.count(); ++i)
+   {
+        temp << X[i] * Y[i];
+   }
+   return temp;
+}
 
 #endif // SINGLEEXPONENTIAL_H
