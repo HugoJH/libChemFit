d33899b71b3e95001ced7e2a3986dda5e2de6e4c
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2018-02-12 16:31:34 +0100

    modified test

--- Tests/tst_singleexponentialtest.cpp
+++ Tests/tst_singleexponentialtest.cpp
@@ -21,11 +21,12 @@ SingleExponentialTest::SingleExponentialTest()
 
 void SingleExponentialTest::testcomputeFirstPreParameter()
 {
+   const double mock = -0.20228711901914495;
    QVector<double> X= {1, 2, 3, 4, 5};
    QVector<double> Y= {1, 3, 5, 7, 9};
    double P1 = SingleExponential::computeFirstPreParameter(X, Y);
    qDebug() << "Computation of single exponential first pre-parameter";
-   QCOMPARE(P1, 5.3);
+   QVERIFY2(qFuzzyCompare(P1, mock), "Parameter calculation failed");
 }
 
 QTEST_APPLESS_MAIN(SingleExponentialTest)
