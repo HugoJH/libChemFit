3cd90bd20a3a955a6a942739b3a6d42679addf18
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2018-02-12 16:50:07 +0100

    Second pre-parameter red test

--- Tests/tst_singleexponentialtest.cpp
+++ Tests/tst_singleexponentialtest.cpp
@@ -12,6 +12,7 @@ class SingleExponentialTest : public QObject
       SingleExponentialTest();
    private Q_SLOTS:
       void testcomputeFirstPreParameter();
+      void testcomputeSecondPreParameter();
 };
 
 SingleExponentialTest::SingleExponentialTest()
@@ -28,6 +29,18 @@ void SingleExponentialTest::testcomputeFirstPreParameter()
    qDebug() << "Computation of single exponential first pre-parameter";
    QVERIFY2(qFuzzyCompare(P1, mock), "Parameter calculation failed");
 }
+
+void SingleExponentialTest::testcomputeSecondPreParameter()
+{
+   const double mock = -0.20228711901914495;
+   QVector<double> X= {1, 2, 3, 4, 5};
+   QVector<double> Y= {1, 3, 5, 7, 9};
+   double P1 = SingleExponential::computeSecondPreParameter(X, Y);
+   qDebug() << "Computation of single exponential second pre-parameter";
+   QVERIFY2(qFuzzyCompare(P1, mock), "Pre-Parameter calculation failed");
+}
+
+
 
 QTEST_APPLESS_MAIN(SingleExponentialTest)
 
