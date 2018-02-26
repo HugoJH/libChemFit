fca470da30df198541200cd2e373725c0aaa9279
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2018-02-26 16:45:47 +0100

    Fixed Single Exponentials tests to use real data

--- Tests/SingleExponentialTest.cpp
+++ Tests/SingleExponentialTest.cpp
@@ -12,10 +12,10 @@ SingleExponentialTest::SingleExponentialTest()
 
 void SingleExponentialTest::testComputeParameters()
 {
-   const double mockP1 = -13.012171317394735;
-   const double mockP2 = 69.75789628400081;
-   QVectorExtended X = {1, 2, 3, 4, 5};
-   QVectorExtended Y = {1, 3, 5, 7, 9};
+   const double mockP1 = 256.9204958141705;
+   const double mockP2 = 0.0015712664939343268;
+   QVector<double> X = {125, 180, 255, 300, 450, 480};
+   QVector<double> Y = {210.52, 192.47, 172.51, 162.61, 125.91, 120.73};
    QPair<double, double> parameters = SingleExponential::computeParameters(X, Y);
    QVERIFY2(qFuzzyCompare(parameters.first, mockP1), "First Parameter calculation failed!");
    QVERIFY2(qFuzzyCompare(parameters.second, mockP2), "Second Parameter calculation failed!");
@@ -24,18 +24,13 @@ void SingleExponentialTest::testComputeParameters()
 void SingleExponentialTest::testComputeSingleExponential()
 {
    QVector<double> X = {125, 180, 255, 300, 450, 480};
-   QVector<double> Y = {210.52,
-                        192.47,
-                        172.51,
-                        162.61,
-                        125.91,
-                        120.73};
-   QVector<double> mockY = {210.7761853607157,
-                            193.4451581468218,
-                            172.0852946676328,
-                            160.4188125663006,
-                            126.9483583244129,
-                            121.1438711325513};
+   QVector<double> Y = {210.52, 192.47, 172.51, 162.61, 125.91, 120.73};
+   QVector<double> mockY = {211.1055764132313,
+                            193.6279846713009,
+                            172.1031017958427,
+                            160.3544590598879,
+                            126.6841220170798,
+                            120.8510463224405};
    QPair<double, double> parameters = SingleExponential::computeParameters(X, Y);
    const double mockP1 = parameters.first;
    const double mockP2 = parameters.second;
@@ -48,60 +43,33 @@ void SingleExponentialTest::testComputeSingleExponential()
 
 void SingleExponentialTest::testComputeExperimentalAreaUnderCurve()
 {
-   QVectorExtended X = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-   QVectorExtended Y = {2,
-                        1.2727168592074,
-                        0.954066036314595,
-                        0.790605289049582,
-                        0.688635684924374,
-                        0.613268606711719,
-                        0.551290388270693,
-                        0.497497185756964,
-                        0.449664426745124,
-                        0.406693069544686,
-                        0.367924841101205};
-   const double mockEAUC = 9.206594219667835;
+   QVector<double> X = {125, 180, 255, 300, 450, 480};
+   QVector<double> Y = {210.52, 192.47, 172.51, 162.61, 125.91, 120.73};
+   const double mockEAUC = 163698.91269979504;
    double EAUC = SingleExponential::computeExperimentalAreaUnderCurve(X,Y);
    QVERIFY2(qFuzzyCompare(EAUC, mockEAUC), "EAUC calculation failed!");
 }
 
 void SingleExponentialTest::testComputeTheoreticalAreaUnderCurve()
 {
-
-   QVectorExtended X = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-   QVectorExtended Y = {2,
-                        1.2727168592074,
-                        0.954066036314595,
-                        0.790605289049582,
-                        0.688635684924374,
-                        0.613268606711719,
-                        0.551290388270693,
-                        0.497497185756964,
-                        0.449664426745124,
-                        0.406693069544686,
-                        0.367924841101205};
-   const double mockTAUC = 8.469526780929929;
+   QVector<double> X = {125, 180, 255, 300, 450, 480};
+   QVector<double> Y = {210.52, 192.47, 172.51, 162.61, 125.91, 120.73};
+   const double mockTAUC = 163511.72560859614;
    double TAUC = SingleExponential::computeTheoreticalAreaUnderCurve(X,Y);
    QVERIFY2(qFuzzyCompare(TAUC, mockTAUC), "TAUC calculation failed!");
 }
 
 void SingleExponentialTest::testComputeAreaUnderCurvePartials()
 {
-   QVectorExtended X = {125, 180, 255, 300, 450, 480};
-   QVectorExtended Y = {210.52,
-                        192.47,
-                        172.51,
-                        162.61,
-                        125.91,
-                        120.73};
-   QVectorExtended mockAUCPartials = {29167.5147228347,
+   QVector<double> X = {125, 180, 255, 300, 450, 480};
+   QVector<double> Y = {210.52, 192.47, 172.51, 162.61, 125.91, 120.73};
+   QVectorExtended mockAUCPartials = {29215.03098838566,
                                       11082.225,
                                       13686.75,
                                       7540.200000000001,
                                       21639,
                                       3699.6,
-                                      77388.51299982953};
-
+                                      76836.10671140937};
 
    QVectorExtended AUCPArtials = SingleExponential::computeAreaUnderCurvePartials(X, Y);
    for(int i = 0; i < AUCPArtials.size(); ++i)
@@ -112,14 +80,9 @@ void SingleExponentialTest::testComputeAreaUnderCurvePartials()
 
 void SingleExponentialTest::testComputeExperimentalVolumeOfDistribution()
 {
-   QVectorExtended X = {125, 180, 255, 300, 450, 480};
-   QVectorExtended Y = {210.52,
-                        192.47,
-                        172.51,
-                        162.61,
-                        125.91,
-                        120.73};
-   const double mockEVoD = 12628.51765482094;
+   QVector<double> X = {125, 180, 255, 300, 450, 480};
+   QVector<double> Y = {210.52, 192.47, 172.51, 162.61, 125.91, 120.73};
+   const double mockEVoD = 12577.045746960894;
    double mockDose = 3.235e6;
    double EVoD = SingleExponential::computeExperimentalVolumeOfDistribution(X, Y, mockDose);
    QVERIFY2(qFuzzyCompare(EVoD, mockEVoD), "EVoD calculation failed!");
@@ -127,14 +90,9 @@ void SingleExponentialTest::testComputeExperimentalVolumeOfDistribution()
 
 void SingleExponentialTest::testComputeTheoreticalVolumeOfDistribution()
 {
-   QVectorExtended X = {125, 180, 255, 300, 450, 480};
-   QVectorExtended Y = {210.52,
-                        192.47,
-                        172.51,
-                        162.61,
-                        125.91,
-                        120.73};
-   const double mockTVoD = 12628.81412042831;
+   QVector<double> X = {125, 180, 255, 300, 450, 480};
+   QVector<double> Y = {210.52, 192.47, 172.51, 162.61, 125.91, 120.73};
+   const double mockTVoD = 12591.443861839118;
    double mockDose = 3.235e6;
    double TVoD = SingleExponential::computeTheoreticalVolumeOfDistribution(X, Y, mockDose);
    QVERIFY2(qFuzzyCompare(TVoD, mockTVoD), "TVoD calculation failed!");
@@ -142,14 +100,9 @@ void SingleExponentialTest::testComputeTheoreticalVolumeOfDistribution()
 
 void SingleExponentialTest::testComputeExperimentalClearance()
 {
-   QVectorExtended X = {125, 180, 255, 300, 450, 480};
-   QVectorExtended Y = {210.52,
-                        192.47,
-                        172.51,
-                        162.61,
-                        125.91,
-                        120.73};
-   const double mockECL = 19.701127174647876;
+   QVector<double> X = {125, 180, 255, 300, 450, 480};
+   QVector<double> Y = {210.52, 192.47, 172.51, 162.61, 125.91, 120.73};
+   const double mockECL = 19.76189057487888;
    double mockDose = 3.235e6;
    double ECL = SingleExponential::computeExperimentalClearance(X, Y, mockDose);
    QVERIFY2(qFuzzyCompare(ECL, mockECL), "ECL calculation failed!");
@@ -157,14 +110,9 @@ void SingleExponentialTest::testComputeExperimentalClearance()
 
 void SingleExponentialTest::testComputeTheoreticalClearance()
 {
-   QVectorExtended X = {125, 180, 255, 300, 450, 480};
-   QVectorExtended Y = {210.52,
-                        192.47,
-                        172.51,
-                        162.61,
-                        125.91,
-                        120.73};
-   const double mockTCL = 19.701589676011327;
+   QVector<double> X = {125, 180, 255, 300, 450, 480};
+   QVector<double> Y = {210.52, 192.47, 172.51, 162.61, 125.91, 120.73};
+   const double mockTCL = 19.78451385036285;
    double mockDose = 3.235e6;
    double TCL = SingleExponential::computeTheoreticalClearance(X, Y, mockDose);
    QVERIFY2(qFuzzyCompare(TCL, mockTCL), "TCL calculation failed!");
@@ -172,14 +120,9 @@ void SingleExponentialTest::testComputeTheoreticalClearance()
 
 void SingleExponentialTest::testComputeHalfLife()
 {
-   QVectorExtended X = {125, 180, 255, 300, 450, 480};
-   QVectorExtended Y = {210.52,
-                        192.47,
-                        172.51,
-                        162.61,
-                        125.91,
-                        120.73};
-   const double mockHL = 444.31068991599864;
+   QVector<double> X = {125, 180, 255, 300, 450, 480};
+   QVector<double> Y = {210.52, 192.47, 172.51, 162.61, 125.91, 120.73};
+   const double mockHL = 441.1391595478878;
    double mockDose = 3.235e6;
    double HL = SingleExponential::computeHalfLife(X, Y, mockDose);
    QVERIFY2(qFuzzyCompare(HL, mockHL), "HL calculation failed!");
@@ -187,14 +130,9 @@ void SingleExponentialTest::testComputeHalfLife()
 
 void SingleExponentialTest::testComputeResidualsSum()
 {
-   QVectorExtended X = {125, 180, 255, 300, 450, 480};
-   QVectorExtended Y = {210.52,
-                        192.47,
-                        172.51,
-                        162.61,
-                        125.91,
-                        120.73};
-   const double mockResidualsSum = 3.9981614450915957;
+   QVector<double> X = {125, 180, 255, 300, 450, 480};
+   QVector<double> Y = {210.52, 192.47, 172.51, 162.61, 125.91, 120.73};
+   const double mockResidualsSum = 3.9062581265964833;
    double residualsSum = SingleExponential::computeResidualsSum(X, Y);
    QVERIFY2(qFuzzyCompare(residualsSum, mockResidualsSum), "Residuals sum calculation failed!");
 }
