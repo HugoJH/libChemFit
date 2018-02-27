b296ffc5a1ed52315d057d8249e57a581b7ba555
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2018-02-27 15:10:40 +0100

    Fixed Double Exponential tests

--- Tests/DoubleExponentialTest.cpp
+++ Tests/DoubleExponentialTest.cpp
@@ -9,19 +9,9 @@ DoubleExponentialTest::DoubleExponentialTest()
 
 void DoubleExponentialTest::testComputeParameters()
 {
-   QVectorExtended X = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-   QVectorExtended Y = {2,
-                        1.2727168592074,
-                        0.954066036314595,
-                        0.790605289049582,
-                        0.688635684924374,
-                        0.613268606711719,
-                        0.551290388270693,
-                        0.497497185756964,
-                        0.449664426745124,
-                        0.406693069544686,
-                        0.367924841101205};
-   QVectorExtended parameterMocks = {0.9999999999999982, 1.000000000000003, 1.000000000000001, 0.1000000000000001};
+   QVectorExtended X = {17, 33, 47, 60, 93, 120, 181, 240, 298, 420};
+   QVectorExtended Y = {140.82, 110.01, 89.35, 76.89, 67.53, 51.57, 38.8, 28.88, 21.9, 12.7};
+   QVectorExtended parameterMocks = {108.7353857611015, 0.03890904159800317, 91.61079628914057, 0.004747428794664105};
    QVectorExtended parameters = DoubleExponential::computeParameters(X, Y);
    QVERIFY2(qFuzzyCompare(parameters[0], parameterMocks[0]), "First parameter calculation failed!");
    QVERIFY2(qFuzzyCompare(parameters[1], parameterMocks[1]), "Second parameter calculation failed!");
@@ -31,22 +21,18 @@ void DoubleExponentialTest::testComputeParameters()
 
 void DoubleExponentialTest::testComputeDoubleExponential()
 {
-   QVectorExtended mockX = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-   QVectorExtended mockY = {2,
-                        1.2727168592074,
-                        0.954066036314595,
-                        0.790605289049582,
-                        0.688635684924374,
-                        0.613268606711719,
-                        0.551290388270693,
-                        0.497497185756964,
-                        0.449664426745124,
-                        0.406693069544686,
-                        0.367924841101205};
-   QVectorExtended mockParameters = {0.9999999999999982,
-                                     1.000000000000003,
-                                     1.000000000000001,
-                                     0.1000000000000001};
+   QVectorExtended mockX = {17, 33, 47, 60, 93, 120, 181, 240, 298, 420};
+   QVectorExtended mockY = {140.6260635110445,
+                            108.4382119807584,
+                            90.75460129565573,
+                            79.4348144190113,
+                            61.82809050720405,
+                            52.84424546416403,
+                            38.8889740749087,
+                            29.32652546500952,
+                            22.26153540252217,
+                            12.47379383493526};
+   QVectorExtended mockParameters = {108.7351890397201, 0.03890892572801488, 91.61073751661439, 0.004747426745796754};
 
    QVectorExtended EstY = DoubleExponential::computeExponential(mockX, mockParameters);
 
@@ -58,58 +44,37 @@ void DoubleExponentialTest::testComputeDoubleExponential()
 
 void DoubleExponentialTest::testComputeExperimentalAreaUnderCurve()
 {
-   QVectorExtended X = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-   QVectorExtended Y = {2,
-                        1.2727168592074,
-                        0.954066036314595,
-                        0.790605289049582,
-                        0.688635684924374,
-                        0.613268606711719,
-                        0.551290388270693,
-                        0.497497185756964,
-                        0.449664426745124,
-                        0.406693069544686,
-                        0.367924841101205};
-   const double mockEAUC = 11.087648378087785;
+   QVectorExtended X = {17, 33, 47, 60, 93, 120, 181, 240, 298, 420};
+   QVectorExtended Y = {140.82, 110.01, 89.35, 76.89, 67.53, 51.57, 38.8, 28.88, 21.9, 12.7};
+   const double mockEAUC = 22384.6098241395;
    double EAUC = DoubleExponential::computeExperimentalAreaUnderCurve(X, Y);
    QVERIFY2(qFuzzyCompare(EAUC, mockEAUC), "EAUC calculation failed!");
 }
 
 void DoubleExponentialTest::testComputeTheoreticalAreaUnderCurve()
 {
-   QVectorExtended X = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-   QVectorExtended Y = {2,
-                        1.2727168592074,
-                        0.954066036314595,
-                        0.790605289049582,
-                        0.688635684924374,
-                        0.613268606711719,
-                        0.551290388270693,
-                        0.497497185756964,
-                        0.449664426745124,
-                        0.406693069544686,
-                        0.367924841101205};
-   const double mockTAUC = 10.999999999999995;
+   QVectorExtended X = {17, 33, 47, 60, 93, 120, 181, 240, 298, 420};
+   QVectorExtended Y = {140.82, 110.01, 89.35, 76.89, 67.53, 51.57, 38.8, 28.88, 21.9, 12.7};
+   const double mockTAUC = 22091.533557822877;
    double TAUC = DoubleExponential::computeTheoreticalAreaUnderCurve(X, Y);
    QVERIFY2(qFuzzyCompare(TAUC, mockTAUC), "TAUC calculation failed!");
 }
 
 void DoubleExponentialTest::testComputeAreaUnderCurvePartials()
 {
-   QVectorExtended X = {125, 180, 255, 300, 450, 480};
-   QVectorExtended Y = {210.52,
-                        192.47,
-                        172.51,
-                        162.61,
-                        125.91,
-                        120.73};
-   QVectorExtended mockAUCPartials = {27916.06208529732,
-                                      11082.225,
-                                      13686.75,
-                                      7540.200000000001,
-                                      21639,
-                                      3699.6,
-                                      76831.9058466466};
+   QVectorExtended X = {17, 33, 47, 60, 93, 120, 181, 240, 298, 420};
+   QVectorExtended Y = {140.82, 110.01, 89.35, 76.89, 67.53, 51.57, 38.8, 28.88, 21.9, 12.7};
+   QVectorExtended mockAUCPartials = {2899.912547427058,
+                                      2006.64,
+                                      1395.52,
+                                      1080.56,
+                                      2382.93,
+                                      1607.85,
+                                      2756.285,
+                                      1996.56,
+                                      1472.62,
+                                      2110.6,
+                                      2675.132276712443};
 
    QVectorExtended AUCPArtials = DoubleExponential::computeAreaUnderCurvePartials(X, Y);
    for(int i = 0; i < AUCPArtials.size(); ++i)
@@ -120,14 +85,9 @@ void DoubleExponentialTest::testComputeAreaUnderCurvePartials()
 
 void DoubleExponentialTest::testComputeExperimentalVolumeOfDistribution()
 {
-   QVectorExtended X = {125, 180, 255, 300, 450, 480};
-   QVectorExtended Y = {210.52,
-                        192.47,
-                        172.51,
-                        162.61,
-                        125.91,
-                        120.73};
-   const double mockEVoD = 6338.828898133474;
+   QVectorExtended X = {17, 33, 47, 60, 93, 120, 181, 240, 298, 420};
+   QVectorExtended Y = {140.82, 110.01, 89.35, 76.89, 67.53, 51.57, 38.8, 28.88, 21.9, 12.7};
+   const double mockEVoD = 3310.3669385811736;
    double mockDose = 3.235e6;
    double EVoD = DoubleExponential::computeExperimentalVolumeOfDistribution(X, Y, mockDose);
    QVERIFY2(qFuzzyCompare(EVoD, mockEVoD), "EVoD calculation failed!");
@@ -135,14 +95,9 @@ void DoubleExponentialTest::testComputeExperimentalVolumeOfDistribution()
 
 void DoubleExponentialTest::testComputeTheoreticalVolumeOfDistribution()
 {
-   QVectorExtended X = {125, 180, 255, 300, 450, 480};
-   QVectorExtended Y = {210.52,
-                        192.47,
-                        172.51,
-                        162.61,
-                        125.91,
-                        120.73};
-   const double mockTVoD = 6295.564985629097;
+   QVectorExtended X = {17, 33, 47, 60, 93, 120, 181, 240, 298, 420};
+   QVectorExtended Y = {140.82, 110.01, 89.35, 76.89, 67.53, 51.57, 38.8, 28.88, 21.9, 12.7};
+   const double mockTVoD = 3354.283762189547;
    double mockDose = 3.235e6;
    double TVoD = DoubleExponential::computeTheoreticalVolumeOfDistribution(X, Y, mockDose);
    QVERIFY2(qFuzzyCompare(TVoD, mockTVoD), "TVoD calculation failed!");
@@ -150,14 +105,9 @@ void DoubleExponentialTest::testComputeTheoreticalVolumeOfDistribution()
 
 void DoubleExponentialTest::testComputeExperimentalClearance()
 {
-   QVectorExtended X = {125, 180, 255, 300, 450, 480};
-   QVectorExtended Y = {210.52,
-                        192.47,
-                        172.51,
-                        162.61,
-                        125.91,
-                        120.73};
-   const double mockECL = 19.920472923700405;
+   QVectorExtended X = {17, 33, 47, 60, 93, 120, 181, 240, 298, 420};
+   QVectorExtended Y = {140.82, 110.01, 89.35, 76.89, 67.53, 51.57, 38.8, 28.88, 21.9, 12.7};
+   const double mockECL = 144.5189362430336;
    double mockDose = 3.235e6;
    double ECL = DoubleExponential::computeExperimentalClearance(X, Y, mockDose);
    QVERIFY2(qFuzzyCompare(ECL, mockECL), "ECL calculation failed!");
@@ -165,14 +115,9 @@ void DoubleExponentialTest::testComputeExperimentalClearance()
 
 void DoubleExponentialTest::testComputeTheoreticalClearance()
 {
-   QVectorExtended X = {125, 180, 255, 300, 450, 480};
-   QVectorExtended Y = {210.52,
-                        192.47,
-                        172.51,
-                        162.61,
-                        125.91,
-                        120.73};
-   const double mockTCL = 19.784511279764796;
+   QVectorExtended X = {17, 33, 47, 60, 93, 120, 181, 240, 298, 420};
+   QVectorExtended Y = {140.82, 110.01, 89.35, 76.89, 67.53, 51.57, 38.8, 28.88, 21.9, 12.7};
+   const double mockTCL = 146.436189752632527;
    double mockDose = 3.235e6;
    double TCL = DoubleExponential::computeTheoreticalClearance(X, Y, mockDose);
    QVERIFY2(qFuzzyCompare(TCL, mockTCL), "TCL calculation failed!");
@@ -180,42 +125,27 @@ void DoubleExponentialTest::testComputeTheoreticalClearance()
 
 void DoubleExponentialTest::testComputeHalfLifeAlphaPhase()
 {
-   QVectorExtended X = {125, 180, 255, 300, 450, 480};
-   QVectorExtended Y = {210.52,
-                        192.47,
-                        172.51,
-                        162.61,
-                        125.91,
-                        120.73};
-   const double mockHLAP = 441.1413991832305;
+   QVectorExtended X = {17, 33, 47, 60, 93, 120, 181, 240, 298, 420};
+   QVectorExtended Y = {140.82, 110.01, 89.35, 76.89, 67.53, 51.57, 38.8, 28.88, 21.9, 12.7};
+   const double mockHLAP = 17.814552918607944;
    double HLAP = DoubleExponential::computeHalfLifeAlphaPhase(X, Y);
    QVERIFY2(qFuzzyCompare(HLAP, mockHLAP), "Half Life Alpha Phase calculation failed!");
 }
 
 void DoubleExponentialTest::testComputeHalfLifeBetaPhase()
 {
-   QVectorExtended X = {125, 180, 255, 300, 450, 480};
-   QVectorExtended Y = {210.52,
-                        192.47,
-                        172.51,
-                        162.61,
-                        125.91,
-                        120.73};
-   const double mockHLBP = 441.11504112192716;
+   QVectorExtended X = {17, 33, 47, 60, 93, 120, 181, 240, 298, 420};
+   QVectorExtended Y = {140.82, 110.01, 89.35, 76.89, 67.53, 51.57, 38.8, 28.88, 21.9, 12.7};
+   const double mockHLBP = 146.00475552977463;
    double HLBP = DoubleExponential::computeHalfLifeBetaPhase(X, Y);
    QVERIFY2(qFuzzyCompare(HLBP, mockHLBP), "Half Life Beta Phase calculation failed!");
 }
 
 void DoubleExponentialTest::testComputeResidualsSum()
 {
-   QVectorExtended X = {125, 180, 255, 300, 450, 480};
-   QVectorExtended Y = {210.52,
-                        192.47,
-                        172.51,
-                        162.61,
-                        125.91,
-                        120.73};
-   const double mockResidualsSum = 3.9062477224197942;
+   QVectorExtended X = {17, 33, 47, 60, 93, 120, 181, 240, 298, 420};
+   QVectorExtended Y = {140.82, 110.01, 89.35, 76.89, 67.53, 51.57, 38.8, 28.88, 21.9, 12.7};
+   const double mockResidualsSum = 3.913766636134053;
    double residualsSum = DoubleExponential::computeResidualsSum(X, Y);
    QVERIFY2(qFuzzyCompare(residualsSum, mockResidualsSum), "Residuals sum calculation failed!");
 }
